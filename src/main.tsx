2b8f7b821bdd4e2db917f5073061a435370f2c7c
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2023-12-26 16:26:37 -0700

    cleaned up code

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 import { useEffect, useRef, useState } from 'react'
 import './index.css'
 
+const addSiteAsAllowed = (url: string, allowedWebsites: string[], setAllowedWebsites:React.Dispatch<React.SetStateAction<string[]>>) => {
+  const urlObject = new URL(url);
+  const baseUrl = `${urlObject.protocol}//${urlObject.hostname}`
+  const newAllowedWebsites = [...allowedWebsites!!, baseUrl];
+  setAllowedWebsites(newAllowedWebsites)
+  chrome.storage.local.set({ 'allowedWebsites': newAllowedWebsites });
+};
+
+const whiteListCurrentSite = (allowedWebsites: string[], setAllowedWebsites: React.Dispatch<React.SetStateAction<string[]>>) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const currentTab = tabs[0];
+    if (currentTab && currentTab.url) {
+      addSiteAsAllowed(currentTab.url, allowedWebsites, setAllowedWebsites)
+    }
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
@@ -18,69 +35,22 @@ enum Page {
 
 function Main({ changePage }: MainProps): JSX.Element {
 
-  const [timeElapsed, setTimeElapsed] = useState<number>(NaN)
-  const [isTimerStopped, setIsTimerStopped] = useState<boolean>(true)
-  const intervalRef = useRef<number>(NaN)
-
-  function stopTimer() {
-    setIsTimerStopped(true)
-    chrome.runtime.sendMessage({ command: 'stopTimer' })
-    clearInterval(intervalRef.current)
-  }
-
-  function startTimer() {
-    chrome.runtime.sendMessage({ command: 'startTimer' })
-    setTimeElapsed(0)
-    unPauseTimer()
-  }
-
-  function unPauseTimer() {
-    chrome.runtime.sendMessage({ command: 'unPauseTimer' })
-    setIsTimerStopped(false)
-    intervalRef.current = setInterval(() => {
-      setTimeElapsed(time => Number.isNaN(time) ? 1 : time + 1)
-    }, 1000)
-  }
+  const [allowedWebsites, setAllowedWebsites] = useState<string[]>([])
 
   useEffect(() => {
+    chrome.storage.local.get(['allowedWebsites'], result => setAllowedWebsites(result.allowedWebsites ?? []))
+  }, []);
 
-    (async () => {
-      const { isTimerStopped } = await chrome.runtime.sendMessage({ command: 'isTimerStopped' })
-      const { timeElapsed } = await chrome.runtime.sendMessage({ command: 'getTime' })
-      setIsTimerStopped(isTimerStopped)
-      setTimeElapsed(timeElapsed)
-
-      if (!isTimerStopped) unPauseTimer()
-    })()
-  }, [])
-
-  return Number.isNaN(timeElapsed) ? (
+  return (
     <>
-      <p className='text-base mx-auto w-fit'>Productivity</p>
-      <p className='text-base'>Time elapsed: 0s</p>
-      <div className='flex items-center mt-[3vh]'>
-        <button style={{ flexGrow: 4 }} className={'text-xl bg-gray-500'}>Start</button>
+      <div className='flex justify-evenly items-center'>
+        <p className='text-base mx-auto w-fit'>Stay productive</p>
         <i onClick={() => changePage('Options')} className="cursor-pointer fa fa-gear text-2xl"></i>
       </div>
-      <button>ADD URL</button>
+      <button className={`'block w-full text-xl ${allowedWebsites ? 'bg-white' : 'bg-gray-500'}`}
+        onClick={() => whiteListCurrentSite(allowedWebsites, setAllowedWebsites)}>Whitelist this site</button>
     </>
   )
-    : (
-      <>
-        <p className='text-base mx-auto w-fit'>Productivity</p>
-        <p className='text-base'>Time elapsed: {timeElapsed}s</p>
-        <div className='flex items-center mt-[3vh] gap-[2vw]'>
-          <button style={{ flexGrow: 1 }} onClick={() => {
-            if (isTimerStopped) {
-              startTimer()
-            } else {
-              stopTimer()
-            }
-          }} className={`text-xl ${isTimerStopped ? 'bg-green-300' : 'bg-red-500'}`}>{isTimerStopped ? "Start" : "Stop"}</button>
-          <i onClick={() => changePage(Page.Options)} className="cursor-pointer fa fa-gear text-2xl"></i>
-        </div>
-      </>
-    )
 }
 
 function Options({ changePage }: { changePage: (page: string) => void }): JSX.Element {
@@ -90,42 +60,28 @@ function Options({ changePage }: { changePage: (page: string) => void }): JSX.El
 
   const newUrlInput = useRef<HTMLInputElement | null>(null)
 
-  const addSiteAsAllowed = (url: string) => {
-    const urlObject = new URL(url);
-    const baseUrl = `${urlObject.protocol}//${urlObject.hostname}`
-    const newAllowedWebsites = [...allowedWebsites, baseUrl];
-    setAllowedWebsites(newAllowedWebsites)
-    chrome.storage.local.set({ 'allowedWebsites': newAllowedWebsites });
-  };
-
   const addSiteFromInput = () => {
     const newSite = newUrlInput.current?.value;
-    if (newSite) addSiteAsAllowed(newSite)
-  }
-
-  const addCurrentTabUrlToStorage = () => {
-    // Use chrome.tabs.query to get the current tab's URL
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const currentTab = tabs[0];
-      if (currentTab && currentTab.url) addSiteAsAllowed(currentTab.url)
-    })
+    if (newSite) addSiteAsAllowed(newSite, allowedWebsites, setAllowedWebsites)
   }
 
   const removeUrl = (index: number) => {
-    const updatedAllowedWebsites = [...allowedWebsites];
+    const updatedAllowedWebsites = [...allowedWebsites!!];
     updatedAllowedWebsites.splice(index, 1); // Remove the item at the specified index
     setAllowedWebsites(updatedAllowedWebsites)
     chrome.storage.local.set({ 'allowedWebsites': updatedAllowedWebsites });
   }
 
   useEffect(() => {
-    chrome.storage.local.get(['allowedWebsites'], result => result.allowedWebsites && setAllowedWebsites(result.allowedWebsites))
+    chrome.storage.local.get(['allowedWebsites'], result => {
+      result.allowedWebsites && setAllowedWebsites(result.allowedWebsites)
+    })
   }, []);
 
   return <>
     <div className='flex justify-between'>
       <p className='text-4xl cursor-pointer' style={{ lineHeight: "2rem" }} onClick={() => changePage(Page.Main)}>&#x2190;</p>
-      <button className='text-lg' onClick={addCurrentTabUrlToStorage}>Add current URL</button>
+      <button className='text-lg' onClick={() =>whiteListCurrentSite(allowedWebsites, setAllowedWebsites)}>Add current URL</button>
     </div>
     <div className='flex justify-between mt-[5vh]'>
       <h2 className='text-lg'>Allowed websites</h2>
